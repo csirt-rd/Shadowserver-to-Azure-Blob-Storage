359210b1a7f3cd8e1e05605e62df4e9d7d5ed2c9
Julio Gonzalez <92810781+jgonzalez-csirtrd@users.noreply.github.com>
2021-11-30 14:50:54 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 from datetime import datetime, timedelta
 import os
+import threading
 from azure.storage.blob import BlobClient
 from azure.core.exceptions import ResourceExistsError
 
-# Azure Data 
+# Azure Storage Blob Info
 AZURE_ACC_KEY = ""
 AZURE_ENDPOINT_SUFFIX = "core.windows.net"
 AZURE_ACC_NAME = ""
@@ -12,47 +13,28 @@ AZURE_ENDPOINT = "{}.table.{}".format(AZURE_ACC_NAME, AZURE_ENDPOINT_SUFFIX)
 AZURE_CONN_STR = "DefaultEndpointsProtocol=https;AccountName={};AccountKey={};EndpointSuffix={}".format(
     AZURE_ACC_NAME, AZURE_ACC_KEY, AZURE_ENDPOINT_SUFFIX
 )
-LOG_FILE = '-'.join([
-    '/'.join([os.getcwd(), 'logs', 'log']),
-    datetime.now().strftime("%Y%m%d")
-]) + '.txt'
 
-def disk_to_blob():
-    MY_PATH = '/'.join([
-        os.getcwd(),
-        'shadowserver'
-    ])
-    try:   
-        os.makedirs('/'.join([os.getcwd(), 'logs']))
-    except FileExistsError:
-        pass
-    for d in os.listdir(MY_PATH):
-        FILE = '-'.join([yesterday.strftime("%Y-%m-%d"), d]) + '.csv'
-        BLOB = os.path.join(d, FILE)
-        SHADOW_FILE = os.path.join(MY_PATH, d, FILE)
-        with open(LOG_FILE, 'a', encoding='utf-8') as l:
-            l.write(
-                ' '.join([
-                    datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S:%f")[:-3],
-                    '+00:00',
-                    '[INF]',
-                    'Uploading',
-                    FILE,
-                    'to Sucuri Blob Storage'
-                    '\n'
-                ])
-            )
-        l.close()
-        with BlobClient.from_connection_string(conn_str=AZURE_CONN_STR, container_name="shadowserver", blob_name=BLOB) as blob:
-            try:
-                with open(SHADOW_FILE, 'rb') as data:
-                    try:
-                        blob.upload_blob(data)
-                    except ResourceExistsError:
-                        pass
-            except FileNotFoundError:
-                pass
+def disk_to_blob(folder, date):
+    FILE = '-'.join([date.strftime("%Y-%m-%d"), folder]) + '.csv'
+    BLOB = os.path.join(folder, FILE)
+    SHADOW_FILE = os.path.join(MY_PATH, folder, FILE)
+    with BlobClient.from_connection_string(conn_str=AZURE_CONN_STR, container_name="shadowserver", blob_name=BLOB) as blob:
+        try:
+            with open(SHADOW_FILE, 'rb') as data:
+                try:
+                    blob.upload_blob(data)
+                except ResourceExistsError:
+                    pass
+        except FileNotFoundError:
+            pass
 
 if __name__ == "__main__":
     yesterday = datetime.now() - timedelta(1)
-    disk_to_blob()
+    MY_PATH = os.path.join(os.getcwd(),'shadowserver')
+    threads = list()
+    for d in os.listdir(MY_PATH):
+        x = threading.Thread(target=disk_to_blob, args=(d, yesterday), daemon=True)
+        threads.append(x)
+        x.start()
+    for index, thread in enumerate(threads):
+        thread.join()
