18059bfb0cfec33393be4157b3f777f2f2c356ee
Julio Gonzalez <92810781+jgonzalez-csirtrd@users.noreply.github.com>
2022-04-07 11:08:37 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import threading, requests, re
+from datetime import datetime, timedelta
 from azure.storage.blob import BlobClient
 from azure.core.exceptions import ResourceExistsError
 
@@ -8,43 +9,45 @@ MATCH = '(https:\/\/dl.shadowserver.org\/[^"]+)'
 SS_URL = "https://dl.shadowserver.org/reports/index.php"
 
 # Azure Storage Blob Info
-AZURE_ACC_KEY = ""
+AZURE_ACC_KEY = "..."
 AZURE_ENDPOINT_SUFFIX = "core.windows.net"
-AZURE_ACC_NAME = ""
+AZURE_ACC_NAME = "..."
 AZURE_ENDPOINT = "{}.table.{}".format(AZURE_ACC_NAME, AZURE_ENDPOINT_SUFFIX)
 AZURE_CONN_STR = "DefaultEndpointsProtocol=https;AccountName={};AccountKey={};EndpointSuffix={}".format(
     AZURE_ACC_NAME, AZURE_ACC_KEY, AZURE_ENDPOINT_SUFFIX
 )
 
-def ss_to_blob(session, url):
-    try:
-        RESPONSE = session.get(url)
-        if RESPONSE.status_code == 200:
-            FILENAME = re.findall("filename=(.+)", RESPONSE.headers['content-disposition'])[0]
-            BLOB = '/'.join([FILENAME[11:-4], FILENAME])
-            with BlobClient.from_connection_string(conn_str=AZURE_CONN_STR, container_name="", blob_name=BLOB) as blob:
+def ss_to_blob(session, url, date):
+    RESPONSE = session.get(url)
+    if RESPONSE.status_code == 200:
+        try:
+            FILENAME = re.findall(f"filename={date}-(.+)", RESPONSE.headers['content-disposition'])[0][:-4]
+        except:
+            pass
+        else:
+            BLOB = '/'.join([FILENAME, '-'.join([date, FILENAME]) + '.csv'])
+            with BlobClient.from_connection_string(conn_str=AZURE_CONN_STR, container_name="...", blob_name=BLOB) as blob:
                 try:
                     blob.upload_blob(RESPONSE.content)
                 except ResourceExistsError:
                     pass
-    except:
-        pass
 
 if __name__ == "__main__":
     SESSION = requests.Session()
     RESPONSE = SESSION.post(
         SS_URL,
         data={
-        'user': "",
-        'password': "",
+        'user': "...",
+        'password': "...",
         'login':'Login'
         }
     )
     HTML_CONTENT = RESPONSE.content.decode('utf-8')
+    yesterday = datetime.now() - timedelta(1)
     threads = list()
-    for download_me in re.finditer(MATCH, HTML_CONTENT, re.MULTILINE):
-        URL = download_me.group(1)
-        x = threading.Thread(target=ss_to_blob, args=(SESSION, URL), daemon=True)
+    URLS = [download_me.group(1) for download_me in re.finditer(MATCH, HTML_CONTENT, re.MULTILINE)][4400:]
+    for URL in URLS:
+        x = threading.Thread(target=ss_to_blob, args=(SESSION, URL, yesterday.strftime("%Y-%m-%d")), daemon=True)
         threads.append(x)
         x.start()
     for index, thread in enumerate(threads):
